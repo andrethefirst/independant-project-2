253f583c01cb0dd1692580070f2412a8334fc2a1
Andre Velasquez <andrethefirst21@gmail.com>
2021-06-20 01:08:15 -0700

    add styling and more work and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-$(document).ready(function) {
+$(document).ready(function); {
   $("form#questions").submit(function(event) {
-    const name = $("input#name").val();
     const answerone = $("select#question-one").val();
     const answertwo = $("select#question-two").val();
     const answerthree = $("select#question-three").val();
@@ -11,18 +10,41 @@ $(document).ready(function) {
     const resulttwo = ("JavaScript");
     const resultthree = ("Python");
 
-    if (answerone === fire && answertwo == mouse && answerthree == lefty && answerfour == gpu && answerfive == month) {
-      Ruby;
+    let finalresult = "Ruby";
+
+    if (answerone === fire && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === month);
+    else {
+
+  
+    if (answerone === water && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === six-months);
+
+    if (answerone === air && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === year);
+
+    if (answerone === earth && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === further);
+    } 
+
+    let finalresult = "JavaScript";
+
+    if (answerone === water && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === six-months)
+    else {
+
+    if (answerone === air && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === year);
+
+    if (answerone === earth && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === further);
     }
-    if (answerone === water && answertwo == keyboard && answerthree == righty && answerfour == ram && answerfive == six-months) {
-      JavaScript;
+
+    let finalresult = "Python";
+
+    if (answerone === air && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === year);
+    else {
+
+    if (answerone === earth && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === further);
+
+    if (answerone === water && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === six-months);
+
+    if (answerone === fire && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === month);
     }
-    if (answerone === air && answertwo == mouse && answerthree == lefty && answerfour == gpu && answerfive == year) {
-      Python;
-    }
-    if (answerone === earth && answertwo == keyboard && answerthree == righty && answerfour == ram && answerfive == further) {
-      Ruby;
-    }
+    
 
     $("#result").text();
     $("finalresult").show();
