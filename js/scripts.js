a1b6fb582da2250902bce118f64ef46629f81c72
Andre Velasquez <andrethefirst21@gmail.com>
2021-06-29 20:26:32 -0700

    finish project

--- js/scripts.js
+++ js/scripts.js
@@ -6,48 +6,38 @@ $(document).ready(function() {
     const answerfour = $("select#question-four").val();
     const answerfive = $("select#question-five").val();
 
-    const resultone = ("Ruby");
-    const resulttwo = ("JavaScript");
-    const resultthree = ("Python");
-
-    let finalresult = ("Ruby");
-
-    if (answerone === fire && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === month);
-    else {
-
+    const resultone = "Ruby";
+    const resulttwo = "JavaScript";
+    const resultthree = "Python";
   
-    if (answerone === water && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === six-months);
-
-    if (answerone === air && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === year);
-
-    if (answerone === earth && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === further);
-    } 
-
-    let finalresult = "JavaScript";
-
-    if (answerone === water && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === six-months)
-    else {
-
-    if (answerone === air && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === year);
-
-    if (answerone === earth && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === further);
-    }
-
-    let finalresult = "Python";
-
-    if (answerone === air && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === year);
-    else {
-
-    if (answerone === earth && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === further);
-
-    if (answerone === water && answertwo === keyboard && answerthree === righty && answerfour === ram && answerfive === six-months);
-
-    if (answerone === fire && answertwo === mouse && answerthree === lefty && answerfour === gpu && answerfive === month);
-    }
+    if (answerone === "fire" && answertwo === "keyboard" && answerthree === "lefty" && answerfour === "ram" && answerfive === "month") {
+      $("#finalresult").text(resultone)
+    } else if (answerone === "water" && answertwo === "keyboard" && answerthree === "righty" && answerfour === "ram" && answerfive === "six-months") {
+      $("#finalresult").text(resultone)
+    } else if (answerone === "air" && answertwo === "mouse" && answerthree === "lefty" && answerfour === "gpu" && answerfive === "year") {
+      $("#finalresult").text(resultone)
+    } else if (answerone === "earth" && answertwo === "keyboard" && answerthree === "righty" && answerfour === "ram" && answerfive === "further") {
+      $("#finalresult").text(resultone)
+    } else if (answerone === "water" && answertwo === "keyboard" && answerthree === "righty" && answerfour === "ram" && answerfive === "six-months") {
+      $("#finalresult").text(resulttwo)
+    } else if (answerone === "air" && answertwo === "mouse" && answerthree === "lefty" && answerfour === "gpu" && answerfive === "year"){
+      $("#finalresult").text(resulttwo)
+    } else if (answerone === "earth" && answertwo === "keyboard" && answerthree === "righty" && answerfour === "ram" && answerfive === "further") {
+      $("#finalresult").text(resulttwo)
+    } else if (answerone === "air" && answertwo === "mouse" && answerthree === "lefty" && answerfour === "gpu" && answerfive === "year") {
+      $("#finalresult").text(resultthree)
+    } else if (answerone === "earth" && answertwo === "keyboard" && answerthree === "righty" && answerfour === "ram" && answerfive === "further") {
+      $("#finalresult").text(resultthree)
+    } else if (answerone === "water" && answertwo === "keyboard" && answerthree === "righty" && answerfour === "ram" && answerfive === "six-months") {
+      $("#finalresult").text(resultthree)
+    } else if (answerone === "fire" && answertwo === "mouse" && answerthree === "lefty" && answerfour === "gpu" && answerfive === "month") {
+      $("#finalresult").text(resultthree)
+    } else {$("#finalresult").text("test")}
+    
     
 
-    $("#result").text();
-    $("finalresult").show();
+    
+    $("#result").show();
     
   event.preventDefault();
   });
